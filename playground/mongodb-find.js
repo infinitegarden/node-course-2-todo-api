c10f1105fe15949f6d83d4569b3a4b8861dd85ba
Lorill Crees <lcrees@2paths.com>
2017-10-24 15:40:40 -0700

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('59efbbb168b399396f206e0d')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos');
+    // });
+
+    // db.collection('Todos').find()
+    //     .count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+        
+    // }, (err) => {
+    //     console.log('Unable to fetch todos');
+    // });
+    db.collection('Users').find({name: 'Jack'})
+    .count().then((count) => {
+    console.log(`Users count: ${count}`);
+    
+}, (err) => {
+    console.log('Unable to fetch todos');
+});
+
+    //db.close();
+
+});
