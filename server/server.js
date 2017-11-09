676cc77b4a401789e465be27127e99b700d5a28b
infinitegarden <lcrees@2paths.com>
2017-11-09 15:10:57 -0800

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -34,10 +34,6 @@ app.get('/todos', (req, res) => {
     });
 });
 
-app.post('/users', (req, res) => {
-
-});
-
 // GET /todos/1234567
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
@@ -120,6 +116,22 @@ app.patch('/todos/:id', (req, res) => {
     
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+
+    var body = _.pick(req.body, ['email', 'password']);   
+    
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+    
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
