89c6525247cf3bc1834d82b99f97d242ff5e8a7a
infinitegarden <lcrees@2paths.com>
2017-11-09 16:13:31 -0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$2Ue9hGXcdLg9YWfz6VKOJuYlgzDGxZH2Pkkf5W/aXUcs2UouRu42a';
+var hashedPassword2 = '$2a$10$3WnwkPm3Nf.mAXkj3.7CAuHnpd1bh9kUXaI1oT9W2Yzi3bJqkKbyi';
+
+bcrypt.compare(password, hashedPassword2, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 
