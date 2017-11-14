356fade8ae010d79899f39b6a1403c90e0b215e0
infinitegarden <lcrees@2paths.com>
2017-11-14 12:09:10 -0800

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,40 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (err || !res) {
+                    reject('Incorrect password');
+                }
+                resolve(user);
+            });
+           
+        });
+    });
+
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+    
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token, 
+        'tokens.access': 'auth'
+    });
+};
+
 UserSchema.pre('save', function (next) {
     var user = this;
     
